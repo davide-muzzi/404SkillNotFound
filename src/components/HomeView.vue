313b8960d8de36e299bb04d5a68a0cde2554ac56
JustDaveHere-School <davidemaximiliano_muzzi@sluz.ch>
2025-06-11 15:42:12 +0200

    style: made HomeView code cleaner

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -3,13 +3,9 @@ import { RouterLink } from "vue-router";
 </script>
 
 <template>
-  <div
-    class="min-h-screen flex flex-col justify-center items-center text-white px-6 pt-[70px]"
-  >
+  <div class="min-h-screen flex flex-col justify-center items-center text-white px-6 pt-[70px]">
     <div class="w-full max-w-6xl space-y-20">
-      <section
-        class="flex flex-col md:flex-row items-center justify-between space-y-10 md:space-y-0"
-      >
+      <section class="flex flex-col md:flex-row items-center justify-between space-y-10 md:space-y-0">
         <div class="text-left">
           <h1 class="text-5xl font-bold mb-3">404SkillNotFound</h1>
           <p class="text-lg">Your #1 Guide to improve you Skills.</p>
@@ -21,17 +17,13 @@ import { RouterLink } from "vue-router";
           class="rounded-2xl w-[50%]"
         />
       </section>
+
       <section>
-        <RouterLink
-          to="/valorant"
-          class="flex flex-col md:flex-row items-center justify-between space-y-6 md:space-y-0 transform transition-transform duration-300 hover:scale-105"
-        >
+        <!-- Valorant -->
+        <RouterLink to="/valorant" class="flex flex-col md:flex-row items-center justify-between space-y-6 md:space-y-0 transform transition-transform duration-300 hover:scale-105">
           <div class="max-w-sm text-left">
             <h2 class="text-2xl font-bold mb-2">Valorant</h2>
-            <p class="text-sm leading-relaxed">
-              Tactical shooter meets high-speed ability warfare - where
-              precision and strategy collide.
-            </p>
+            <p class="text-sm leading-relaxed">Tactical shooter meets high-speed ability warfare - where precision and strategy collide.</p>
           </div>
 
           <img
@@ -41,13 +33,10 @@ import { RouterLink } from "vue-router";
           />
         </RouterLink>
       </section>
-      <section
-        class="mt-16 mb-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 text-white text-center max-w-6xl mx-auto px-6"
-      >
-        <RouterLink
-          to="/rocketleague"
-          class="transform transition-transform duration-300 hover:scale-105"
-        >
+      <section class="mt-16 mb-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 text-white text-center max-w-6xl mx-auto px-6">
+
+      <!-- Rocket League -->
+        <RouterLink to="/rocketleague" class="transform transition-transform duration-300 hover:scale-105">
           <div>
             <img
               src="/src/assets/rocketleague_homepage.jpg"
@@ -55,18 +44,12 @@ import { RouterLink } from "vue-router";
               class="w-48 h-48 mx-auto rounded-xl mb-3 object-cover"
             />
             <h3 class="text-lg font-semibold">Rocket League</h3>
-            <p class="text-sm text-gray-300">
-              Where aerial flips decide victory
-            </p>
+            <p class="text-sm text-gray-300">Where aerial flips decide victory</p>
           </div>
         </RouterLink>
 
         <!-- Lethal Company -->
-
-        <RouterLink
-          to="/lethalcompany"
-          class="transform transition-transform duration-300 hover:scale-105"
-        >
+        <RouterLink to="/lethalcompany" class="transform transition-transform duration-300 hover:scale-105">
           <div>
             <img
               src="/src/assets/lethalcompany_homepage.jpg"
@@ -79,10 +62,7 @@ import { RouterLink } from "vue-router";
         </RouterLink>
 
         <!-- R.E.P.O. -->
-        <RouterLink
-          to="/repo"
-          class="transform transition-transform duration-300 hover:scale-105"
-        >
+        <RouterLink to="/repo" class="transform transition-transform duration-300 hover:scale-105">
           <div>
             <img
               src="/src/assets/repo_homepage.png"
