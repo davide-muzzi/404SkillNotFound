37981d2e280c19e826835f2cdbce817735123a0e
Davide Muzzí <davidemax.muzzi@gmail.com>
2025-06-11 23:45:29 +0200

    style(fonts): apply fonts to about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,27 +1,23 @@
 <template>
   <div class="pt-24 min-h-screen px-6">
-    <div class="max-w-4xl mx-auto">
-      <h1 class="text-4xl font-extrabold text-white mb-6 tracking-tight text-center">
-        404SkillNotFound
-      </h1>
+    <div class="max-w-4xl mx-auto font-rajdhani">
+      <h1 class="text-5xl font-extrabold text-white mb-12 tracking-tight text-center">404SkillNotFound</h1>
 
-      <p class="text-lg text-gray-300 mb-12 text-center">
+      <p class="text-xl text-gray-300 mb-12 text-center">
         Your new favorite hub for improving your gameplay across a variety of strategy and team-based games.
         
       </p>
       
-        <p class="text-gray-300 text-lg mb-12 ml-10">
-          We created this platform with one goal: 
-Bring together scattered gaming knowledge into one clear, helpful, and easy-to-use site.
-
-Whether you're looking for tips, lineups, mechanics, or game-specific guides – we've got you covered.
+        <p class="text-gray-300 text-xl mb-12 ml-7 mr-5">
+          We created this platform with one goal:
+          <span class="font-semibold text-white">Bring together scattered gaming knowledge into one clear, helpful, and easy-to-use site.</span>
+          Whether you're looking for tips, lineups, mechanics, or game-specific guides - we've got you covered.
         </p>
-      <br />
 
       <!-- Our Mission -->
       <section class="mb-12 ml-10">
-        <h2 class="text-2xl font-bold text-white mb-2 ">Our Mission</h2>
-        <ul class="list-disc list-inside text-gray-300 space-y-1 pl-4">
+        <h2 class="text-3xl font-bold text-white mb-2 ">Our Mission</h2>
+        <ul class="list-disc list-inside text-gray-300 space-y-1 pl-4 text-lg">
           <li>Clear and up-to-date guides to improve your skills</li>
           <li>A clean, user-friendly website experience</li>
           <li>A place to share and explore community-created game templates</li>
@@ -31,8 +27,8 @@ Whether you're looking for tips, lineups, mechanics, or game-specific guides –
 
       <!-- What Makes Us Different -->
       <section class="mb-12 ml-10">
-        <h2 class="text-2xl font-bold text-white mb-2">What Makes Us Different?</h2>
-        <ul class="list-disc list-inside text-gray-300 space-y-1 pl-4">
+        <h2 class="text-3xl font-bold text-white mb-2">What Makes Us Different?</h2>
+        <ul class="list-disc list-inside text-gray-300 space-y-1 pl-4 text-lg">
           <li>Community-Driven Ideas: Suggest and rate game templates</li>
           <li>Customizable Layouts: Dark mode, favorites, and personal flair</li>
           <li>Optional Premium Access: For deep-dive guides and exclusive content</li>
@@ -42,16 +38,16 @@ Whether you're looking for tips, lineups, mechanics, or game-specific guides –
 
       <!-- Who Are We -->
       <section class="mb-12 ml-10">
-        <h2 class="text-2xl font-bold text-white mb-2">Who Are We?</h2>
-        <p class="text-gray-300">
-          We’re two students, turning our passion for gaming and development into a real product. Backed by a clear project plan, real stakeholder analysis, and risk management, this isn't just a school project – it’s a growing platform for gamers, by gamers.
+        <h2 class="text-3xl font-bold text-white mb-2">Who Are We?</h2>
+        <p class="text-gray-300 text-lg">
+          We're two students, turning our passion for gaming and development into a real product. Backed by a clear project plan, real stakeholder analysis, and risk management, this isn't just a school project - it's a growing platform for gamers, by gamers.
         </p>
       </section>
 
       <!-- Get Involved -->
       <section class="mb-16 ml-10">
-        <h2 class="text-2xl font-bold text-white mb-2">Want to Get Involved?</h2>
-        <p class="text-gray-300">
+        <h2 class="text-3xl font-bold text-white mb-2">Want to Get Involved?</h2>
+        <p class="text-gray-300 text-lg">
           Share your own tips, rate existing guides, or just enjoy the content.
           <br />
           <span class="font-semibold text-white">Together, we grow better at the games we love.</span>
