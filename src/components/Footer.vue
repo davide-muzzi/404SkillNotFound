e05f8a8cc6c97348eec7799635ce7cc7608d1569
JustDaveHere-School <davidemaximiliano_muzzi@sluz.ch>
2025-06-12 12:02:06 +0200

    style(footer): add a bit of margin on top of the footer to seperate it more clearly

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <footer class="pt-[40px] bottom-0"> 
+  <footer class="pt-[40px] bottom-0 mt-10"> 
     <p class="text-white flex justify-center font-extrabold text-xl font-rajdhani">404SkillNotFound</p>
     <div class="flex justify-between p-8 text-white">
       <div class="text-m font-rajdhani">Contact</div>
